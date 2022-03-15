451d7b0a6bccca1787a46e76b99d6f16daf39c76
Sergey <lyamkin.serezha@mail.ru>
2022-03-16 01:34:04 +0700

    add hw1

--- src/code.cpp
+++ src/code.cpp
@@ -38,56 +38,80 @@ namespace assignment {
 
   // Task 4
   void swap_args(int *left, int *right) {
-
-    if (left != nullptr or right != nullptr)
-      std::cout << "left = " << right << ", right = " << left;
-    else {
-      std::cout << "left = " << left << ", right = " << right;
+    if (left != nullptr && right != nullptr) {
+      int temp = *left;
+      *left = *right;
+      *right = temp;
     }
   }
 
   // Task 5
   int arr_sum(int *arr, int length) {
-
-    int sum_ = 0;
-    if (arr != 0 or length <= 0) {
-      for (int i = 0; i <= length; i++) {
+    if(arr != nullptr && length > 0) {
+      int sum_ = 0;
+      for (int i = 0; i < length; i++) {
         sum_ += arr[i];
       }
       return sum_;
     }
-
     return 0;
   }
 
   // Task 6
   int *find_max_elem(int *arr, int length) {
-
-    // Write your code here ...
-
+    if (arr != nullptr && length > 0) {
+      int *max_ = &arr[0];
+      for (int i = 0; i < length; i++) {
+        if (arr[i] > *max_) {
+          max_ = &arr[i];
+        }
+      }
+      return max_;
+    }
     return nullptr;
   }
 
   // Task 7
   int *allocate_arr(int length, int init_value) {
-
-
-
+  if(length > 0) {
+    int* arr = new int[length];
+    std::fill(arr, arr + length, init_value);
+    return arr;
+    }
     return nullptr;
   }
 
   // Task 8
   int *clone_arr(int *arr_in, int length) {
-
-    // Write your code here ...
-
+    if(length > 0 && arr_in != nullptr) {
+      int *clone_arr = new int[length];
+      std::copy(arr_in, arr_in + length, clone_arr);
+      return clone_arr;
+    }
     return nullptr;
   }
 
   // Task 9
   void print_kth_elements(int *arr, int length, int k, std::ostream &os) {
-
-    // Write your code here ...
+    if(length <= 0) {
+      os << "Invalid argument: length\\n";
+    }
+    else if(arr == nullptr) {
+      os << "Invalid argument: arr\\n";
+    }
+    else if(k <= 0) {
+      os << "Invalid argument: k\\n";
+    }
+    else if(length > 0 && arr != nullptr && k > 0) {
+      for(int i = 0; i < length; i++) {
+        if(k == 1) {
+          os << arr[i] << "\t";
+        }
+        else if(i % k == 0) {
+          os << arr[i] << "\t";
+        }
+      }
+    }
   }
 
 }  // namespace assignment
